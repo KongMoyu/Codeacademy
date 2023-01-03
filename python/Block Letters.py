f479ceacbd5f7e35702e6d43ed39c2ed598d3b77
Mr.Kong <115706052+KongMoyu@users.noreply.github.com>
2023-01-03 21:35:59 +0800

    Create Block Letters.py

---
+++ python/Block Letters.py
@@ -0,0 +1,20 @@
+First_initial = """ 
+K   K
+K  K
+K K
+KK
+K K
+K  K 
+K   K
+"""
+ 
+Second_initial = """ 
+M   M
+MM MM
+MM MM
+M M M
+M   M
+M   M
+M   M
+"""
+print(First_initial+Second_initial)
