38ca890055f0d462b9f4de6a7a390f37d8e55416
Mr.Kong <115706052+KongMoyu@users.noreply.github.com>
2023-01-03 21:29:35 +0800

    Create Block Letters.cpp

---
+++ c++/Block Letters.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+  std::cout << "  SSS   L     \n";
+  std::cout << " S   S  L     \n";
+  std::cout << " S      L     \n";
+  std::cout << "  SSS   L     \n";
+  std::cout << "     S  L     \n";
+  std::cout << " S   S  L     \n";
+  std::cout << "  SSS   LLLLL \n";
+}
